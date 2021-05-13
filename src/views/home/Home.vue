33fa618dfa2a3149256c6813ad35ef69c3b86fcc
呵呵呵 <2502016524@qq.com>
2021-05-13 16:43:17 +0800

    轮播图

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -5,28 +5,47 @@
         <div>购物街</div>
       </div>
     </nav-bar>
+    <swiper>
+      <swiper-item v-for="item in banners">
+          <img :src="item.image" alt="">
+      </swiper-item>
+    </swiper>
   </div>
 </template>
 
 <script>
 import NavBar from "components/common/navbar/NavBar";
 import {getHomeMultidata} from "network/home";
+import {Swiper, SwiperItem} from 'components/common/swiper/index'
 
 export default {
   name: "Home",
+  data() {
+    return {
+      banners: [],
+      recommends:[]
+    }
+  },
   components: {
-    NavBar
+    NavBar,
+    Swiper,
+    SwiperItem
   },
   created() {
-    getHomeMultidata().then(res=>{
-      console.log(res)
+    getHomeMultidata().then(res => {
+      this.banners = res.data.data.banner.list
+      /*this.banners[0].image = "assets/img/xjw/4.jpg"
+      this.banners[1].image = "assets/img/xjw/5.jpg"
+      this.banners[2].image = "assets/img/xjw/6.jpg"
+      this.banners[3].image = "assets/img/xjw/7.jpg"*/
+      this.recommends = res.data.data.recommend.list
     })
   }
 }
 </script>
 
 <style scoped>
-.home-nav{
+.home-nav {
   background-color: var(--color-tint);
   color: white;
 }
